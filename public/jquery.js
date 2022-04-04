73e0bc42b7038a11af9404170832371dba9d3c77
Quyen Nguyen <quyen.ng1902@gmail.com>
2022-04-04 15:48:14 +0300

    burger menu, category JS, cart button, pop up detail view

---
+++ public/jquery.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById('myNav').style.height = '100%';
+  // document.querySelector('.left-nav-btns').style.display = 'unset';
+}
+
+function closeNav() {
+  document.getElementById('myNav').style.height = '0%';
+}
