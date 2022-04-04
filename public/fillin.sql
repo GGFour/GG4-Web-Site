58927f9ec08eb7ecf6c171b5ddd8daa74b163a7d
pochapali <ivantrufan27@gmail.com>
2022-04-04 13:40:14 +0300

    Made items added to shop page using js file with data and made pop up to appear

---
+++ public/fillin.sql
@@ -0,0 +1,8 @@
+INSERT INTO item_category (name, description) VALUES ("armor", "");
+INSERT INTO item_category (name, description) VALUES ("weapon", "");
+INSERT INTO item_category (name, description) VALUES ("key", "");
+INSERT INTO item_category (name, description) VALUES ("wand", "");
+INSERT INTO item_category (name, description) VALUES ("armor", "");
+INSERT INTO item_category (name, description) VALUES ("weapon", "");
+INSERT INTO item_category (name, description) VALUES ("key", "");
+INSERT INTO item_category (name, description) VALUES ("wand", "");
---
+++ public/shop.js
@@ -0,0 +1,37 @@
+const items = require("./items");
+console.log();
+for (let i = 0; i < items.items.length; i++) {
+  addItem(i);
+}
+function addItem(index) {
+  ///just adding items in detail view using data from variable
+  let newElement = document.createElement("div");
+  newElement.innerHTML = `
+    <div class="image">
+      <img src="./images/cat.jpg">
+    </div>
+    <div class="name">${items.items[index].name}</div>
+    <div class="price">69€</div>
+  `;
+  newElement.className = "item";
+  newElement.id = index;
+  newElement.onclick = () => showDetailView(items.items[index], index);
+  document.getElementsByClassName("products-grid")[0].appendChild(newElement);
+}
+function showDetailView(item, index) {
+  let newElement = document.createElement("div");
+  newElement.innerHTML = `
+    <div class="image">
+        <img src="./images/cat.jpg">
+    </div>
+    <div class="name">${item.name}</div>
+    <div class="price">69€</div>
+    <div class="description">${item.description}</div>
+    `;
+  newElement.className = "detailedItem";
+  newElement.id = index;
+  document
+    .getElementsByClassName("products-grid")[0]
+    .getElementById(index)
+    .appendChild(newElement);
+}
