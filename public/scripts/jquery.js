b9f85f0f9e7f62d0285eb611d3f33833d4dcb274
pochapali <ivantrufan27@gmail.com>
2022-04-13 14:21:09 +0300

    Made detailed popups to show and hide without changing design :heavy_check_mark:

--- public/scripts/jquery.js
+++ public/scripts/jquery.js
@@ -1,3 +1,46 @@
+// function openNav() {
+//   document.getElementById("myNav").style.height = "100%";
+//   // document.querySelector('.left-nav-btns').style.display = 'unset';
+// }
+
+// function closeNav() {
+//   document.getElementById("myNav").style.height = "0%";
+// }
+
+// // const detailPopup = document.getElementsByClassName(".detail-popup");
+// // console.log(detailPopup);
+// // console.log(document.getElementsByClassName("clickable-img"));
+
+// // const clickableImg = $(".clickable-img");
+// //damn, it doesn't work, I can't access individual image
+
+// /// line before it was querySelector, the thing is query selector takes only 1 element with according name,
+// /// but we need to toggle more than 1 element so I'm using getElementsByClassName
+// // console.log(clickableImg);
+
+// function showDetailPopup() {
+//   $(".detail-popup").toggle("show-detail-popup");
+// }
+// function windowClicked(event) {
+//   if (event.target === $(".detail-popup")) {
+//     showDetailPopup();
+//   }
+// }
+// $(".clickable-img").addEventListener("click", showDetailPopup);
+// window.addEventListener("click", windowClicked);
+
+// // const Cart = document.getElementById("myCart");
+// // function openCart() {
+// //   document.getElementById("myCart").style.width = "50%";
+// // }
+
+// function openCart() {
+//   const Cart = document.getElementById("myCart");
+//   const inner = document.querySelector(".inner-container");
+//   Cart.classList.toggle("show-main-container");
+//   inner.classList.toggle("show-inner-container");
+// }
+
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
   // document.querySelector('.left-nav-btns').style.display = 'unset';
--- public/scripts/shop.js
+++ public/scripts/shop.js
@@ -1,24 +1,77 @@
 // const items = require("./items");
 const itemTemplate = document.querySelector("[item-template]");
-console.log(itemTemplate);
+// console.log(itemTemplate);
 //dunno why there are `[]`, saw it on youtube, cool video
+let detailPopupId = null;
+
 fetch("/api/items")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((item) => {
       const itemElement = itemTemplate.content.cloneNode(true).children[0];
-      console.log(itemElement);
+      // console.log(itemElement);
       const name = itemElement.querySelector(".name");
-      console.log(name);
+      // console.log(name);
       const price = itemElement.querySelector(".price");
-
       name.textContent = item.name;
       price.textContent = item.price;
-      console.log(document.querySelector(".products-grid"));
+      itemElement.querySelector(".price-popup").textContent = item.price;
+      itemElement.querySelector(".name-popup").textContent = item.name;
+      itemElement.querySelector(".description-popup").textContent =
+        item.description;
+      itemElement.querySelector(".detail-popup").id = item.id;
+      itemElement.querySelector(".clickable-img").addEventListener(
+        "click",
+        () => {
+          itemElement
+            .querySelector(".detail-popup")
+            .classList.toggle("show-detail-popup");
+          detailPopupId = itemElement.querySelector(".detail-popup").id;
+        }
+        //where the hell did you find "show-detail-popup"? it works
+      );
+
+      // console.log(document.querySelector(".products-grid"));
       document.querySelector(".products-grid").append(itemElement);
     });
   });
+// function windowClicked(event) {
+//   if (event.target === detailPopup) {
+//     showDetailPopup();
+//   }
+// }
+console.log(document.getElementById(detailPopupId), detailPopupId);
+window.addEventListener("click", function (event) {
+  console.log(document.getElementById(detailPopupId));
+  if (event.target == document.getElementById(detailPopupId)) {
+    {
+      console.log(detailPopupId);
+      document
+        .getElementById(detailPopupId)
+        .classList.remove("show-detail-popup");
+    }
+  }
+});
 
+function showDetailPopup() {
+  document.getElementsByClassName("popup")[0].classList.remove("js-is-hidden");
+}
+//just copied from jquery.js
+function openCart() {
+  const Cart = document.getElementById("myCart");
+  const inner = document.querySelector(".inner-container");
+  Cart.classList.toggle("show-main-container");
+  inner.classList.toggle("show-inner-container");
+}
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+  // document.querySelector('.left-nav-btns').style.display = 'unset';
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+///didn't work with code below yet
 function leaveOneCategory(category) {
   for (let i = 0; i < items.categories.length; i) {
     if (category !== items.categories[i]) {
