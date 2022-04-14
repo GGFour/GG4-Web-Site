5cab47bee0c14303513b7eecafd7217982ccc3be
pochapali <ivantrufan27@gmail.com>
2022-04-14 11:14:53 +0300

    Okay, so I made functionality to make only one category visible, but it doesn't change position of item :hankey:

--- public/scripts/shop.js
+++ public/scripts/shop.js
@@ -15,10 +15,12 @@ fetch("/api/items")
       const price = itemElement.querySelector(".price");
       name.textContent = item.name;
       price.textContent = item.price;
+      itemElement.classList.add(`${item.category}`);
       itemElement.querySelector(".price-popup").textContent = item.price;
       itemElement.querySelector(".name-popup").textContent = item.name;
       itemElement.querySelector(".description-popup").textContent =
         item.description;
+
       itemElement.querySelector(".detail-popup").id = item.id;
       itemElement.querySelector(".clickable-img").addEventListener(
         "click",
@@ -56,7 +58,7 @@ window.addEventListener("click", function (event) {
 function showDetailPopup() {
   document.getElementsByClassName("popup")[0].classList.remove("js-is-hidden");
 }
-//just copied from jquery.js
+//just copied from jquery.js, it works well
 function openCart() {
   const Cart = document.getElementById("myCart");
   const inner = document.querySelector(".inner-container");
@@ -72,60 +74,65 @@ function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
 ///didn't work with code below yet
+////////
 function leaveOneCategory(category) {
-  for (let i = 0; i < items.categories.length; i) {
-    if (category !== items.categories[i]) {
-      hideCategory(items.categories[i]);
-    } else {
-      showCategory(items.categories[i]);
-    }
-  }
-}
-function showAll() {
-  document.getElementsByClassName("item").classlist.remove("js-is-hidden"); //not sure about that, maybe need to use for loop
-}
-function hideCategory(category) {
-  document.getElementsByClassName("category").classlist.add("js-is-hidden");
-}
-function showCategory(category) {
-  document.getElementsByClassName("category").classlist.remove("js-is-hidden");
-}
-function addItem(index) {
-  ///just adding items in detail view using data from variable
-  let newElement = document.createElement("div");
-  newElement.innerHTML = `
-    <div class="image">
-      <img src="./images/cat.jpg">
-    </div>
-    <div class="name">${items.items[index].name}</div>
-    <div class="price">69€</div>
-  `;
-  newElement.className = `item ${items.items[index].category}`; ///here we should edit to make class and category match, the hell i just typed
-  newElement.id = index;
-  newElement.onclick = () => showDetailView(items.items[index], index);
-  document.getElementsByClassName("products-grid")[0].appendChild(newElement);
-}
-function showDetailView(item, index) {
-  let newElement = document.createElement("div");
-  newElement.innerHTML = `
-    <div class="image">
-        <img src="./images/cat.jpg">
-    </div>
-    <div class="name">${item.name}</div>
-    <div class="price">69€</div>
-    <div class="description">${item.description}</div>
-    <button class="btn btn-primary shop-item-add" type="button" id=${index}>Add to cart</button>
-    `;
-  newElement.className = "detailedItem";
-  newElement.id = index;
-
-  document
-    .getElementsByClassName("products-grid")[0]
-    .getElementById(index)
-    .appendChild(newElement);
-  document.addEventListener("click", function (event) {
-    if (!event.target.closest(".detailedItem")) {
-      document.getElementsByClassName("detailedItem")[0].remove();
-    }
+  document.querySelectorAll(`.grid-div`).forEach(function (el) {
+    // console.log(el);
+    el.style.visibility = "hidden";
+  });
+  document.querySelectorAll(`.${category}`).forEach(function (el) {
+    console.log(el);
+    el.style.visibility = "visible";
   });
 }
+function showAll() {
+  document.getElementsByClassName("item").classlist.remove("js-is-hidden");
+  //not sure about that, maybe need to use for loop
+}
+function hideCategory(category) {
+  document.getElementsByClassName(`${category}`).classlist.add("js-is-hidden");
+}
+function showCategory(category) {
+  document
+    .getElementsByClassName(`${category}`)
+    .classlist.remove("js-is-hidden");
+}
+// function addItem(index) {
+//   ///just adding items in detail view using data from variable
+//   let newElement = document.createElement("div");
+//   newElement.innerHTML = `
+//     <div class="image">
+//       <img src="./images/cat.jpg">
+//     </div>
+//     <div class="name">${items.items[index].name}</div>
+//     <div class="price">69€</div>
+//   `;
+//   newElement.className = `item ${items.items[index].category}`; ///here we should edit to make class and category match, the hell i just typed
+//   newElement.id = index;
+//   newElement.onclick = () => showDetailView(items.items[index], index);
+//   document.getElementsByClassName("products-grid")[0].appendChild(newElement);
+// }
+// function showDetailView(item, index) {
+//   let newElement = document.createElement("div");
+//   newElement.innerHTML = `
+//     <div class="image">
+//         <img src="./images/cat.jpg">
+//     </div>
+//     <div class="name">${item.name}</div>
+//     <div class="price">69€</div>
+//     <div class="description">${item.description}</div>
+//     <button class="btn btn-primary shop-item-add" type="button" id=${index}>Add to cart</button>
+//     `;
+//   newElement.className = "detailedItem";
+//   newElement.id = index;
+
+// document
+//   .getElementsByClassName("products-grid")[0]
+//   .getElementById(index)
+//   .appendChild(newElement);
+// document.addEventListener("click", function (event) {
+//   if (!event.target.closest(".detailedItem")) {
+//     document.getElementsByClassName("detailedItem")[0].remove();
+//   }
+// });
+// }
